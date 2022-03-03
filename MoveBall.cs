15ac8bac2abe84b22f8ba9e8294c43b5ccd3bc72
ZahraTavakoli <100377681+ZAHRATAVAKoLE@users.noreply.github.com>
2022-03-03 10:44:37 +0330

    Add files via upload

---
+++ MoveBall.cs
@@ -0,0 +1,30 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    float speed = 20;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D mihman)
+    {
+        if (mihman.gameObject.name == "Wallleft")
+        {
+
+        }
+        else if (mihman.gameObject.name == "Wallright")
+        {
+
+        }
+    }
+}
---
+++ MoveRocket.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRocket : MonoBehaviour
+{
+    float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // «?‰ ?ò ò«„‰  «”  Ê Â?ç  «À?—? œ— «Ã—«? »—‰«„Â ‰œ«—œ
+        // UP or W : 1
+        // DOWN or S : -1
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
